c266f5d1c2f4bad39a9feb3025e75e2518e0d89c
gkibria121 <gkibria121@gmail.com>
2025-08-27 19:12:23 +0600

    add docker compose

--- next.config.mjs
+++ next.config.mjs
@@ -1,56 +1,57 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'via.placeholder.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'source.unsplash.com',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-            }, 
-            {
-                protocol: 'https',
-                hostname: 'localhost:8000',
-            }, 
-            {
-                protocol: 'http',
-                hostname: 'gkibria121.com:8000',
-            },
-             
-            {
-                protocol: 'http',
-                hostname: 'gkibria121.com',
-            },
-            {
-                protocol: 'http',
-                hostname: '192.168.0.6',
-            },
-            
-            {
-                protocol: 'https',
-                hostname: 'unitechholdingsltd.com',
-            },
-            
-            {
-                protocol: 'http',
-                hostname: 'unitech.jumpintojob.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'unitech.jumpintojob.com',
-            },
-        ],
-    },
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost:8000",
+      },
+      {
+        protocol: "http",
+        hostname: "gkibria121.com:8000",
+      },
+
+      {
+        protocol: "http",
+        hostname: "gkibria121.com",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.0.6",
+      },
+
+      {
+        protocol: "https",
+        hostname: "unitechholdingsltd.com",
+      },
+
+      {
+        protocol: "http",
+        hostname: "unitech.jumpintojob.com",
+      },
+      {
+        protocol: "https",
+        hostname: "unitech.jumpintojob.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
